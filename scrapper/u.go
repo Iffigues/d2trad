e560088587b9aed22120ea4132e2a0046040137f
iffigues <iffigues@vivaldi.net>
2021-11-02 00:15:53 +0100

    erase

--- scrapper/u.go
+++ scrapper/u.go
@@ -26,7 +26,49 @@ func collectText(n *html.Node, buf *bytes.Buffer) {
     }
 }
 
-func h(e string n *html.Node) {
+func collectImg(n *html.Node, buf *bytes.Buffer) {
+    if n.Type == html.ElementNode && n.Data == "img" {
+	for _,a := range n.Attr {
+		if a.Key == "src" {
+			buf.WriteString(a.Val)
+		}
+	}
+    }
+    for c := n.FirstChild; c != nil; c = c.NextSibling {
+        collectImg(c, buf)
+    }
+}
+
+func h(e string, cc *html.Node) {
+	i := 0
+	var name string
+	var char string
+	var img string
+	var f func(*html.Node)
+	f = func(n *html.Node ) {
+		if n.Type == html.ElementNode && n.Data == "td" {
+			if i == 1 {
+				text := &bytes.Buffer{}
+				collectImg(n, text)
+				img = text.String()
+			} else  {
+				text := &bytes.Buffer{}
+				collectText(n, text)
+				if i == 0 {
+					name = text.String()
+				}
+				if i == 2 {
+					char = text.String()
+				}
+			}
+			i = i + 1
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(cc)
+	fmt.Println(name, char, img)
 }
 
 func uniques(e Obj) {
@@ -38,7 +80,6 @@ func uniques(e Obj) {
 	}
 	var f func(*html.Node)
 	var tt string
-	fmt.Println(e.Url)
 	f = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "div" {
 			var  bb bool
@@ -53,27 +94,20 @@ func uniques(e Obj) {
 				collectText(n, text)
 				if text.String() == "Normaux" || text.String() == "Normales" || text.String() == "normales" {
 					tt = "Normaux"
-					print("normal\n")
+					//print("normal\n")
 				}
 				if text.String() == "Exceptionnels" || text.String() == "Exceptionnelles" || text.String() == "EXCEPTIONNELleS"{
 					tt = "Exceptionnels"
-					print("exceptionnel\n")
+					//print("exceptionnel\n")
 				}
 				if text.String() == "Élites" || text.String() == "éLITES" {
 					tt = "Élites"
-					print("elite\n")
+					//print("elite\n")
 				}
 			}
 		}
-		if n.Type == html.ElementNode && n.Data == "div" {
-			var b bool
-			for _, a := range n.Attr {
-				if a.Key == "class" && a.Val == "et_pb_text_inner" {
-					b = true
-				}
-				if b {
-				}
-			}
+		if n.Type == html.ElementNode && n.Data == "table" {
+			h(tt, n)
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			f(c)
